5644db09f2bb4ce05a63da9505814029307c1f9b
Ali-Altabtabae <78157385+Ali-Altabtabae@users.noreply.github.com>
2021-02-02 18:18:24 +0300

    Solved Task 4

--- calculator.js
+++ calculator.js
@@ -1,3 +1,14 @@
 // Define the numbers and the operation here
-
+let firstNum = 18;
+let secondNum = 6;
+let mathOp = "/";
 // Write if-statements here
+if (mathOp == "+") {
+    console.log(firstNum + secondNum);
+} else if (mathOp == "-") {
+    console.log(firstNum - secondNum);
+} else if (mathOp == "*") {
+    console.log(firstNum * secondNum);
+} else if (mathOp == "/") {
+    console.log(firstNum / secondNum);
+}
